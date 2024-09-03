00d465e81919e5b585eb9c39a6eff7f06b4228a7
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-09-03 15:53:43 +0800

    Update _run_server.py

--- _run_server.py
+++ _run_server.py
@@ -7,5 +7,6 @@ _init_config_()
 import start_point as sp
 _init_config_()
 
+
 app = sp.app
 # app.run(host=c.CURRENT_SETTING['HOST'], port=c.CURRENT_SETTING['PORT'], debug=c.CURRENT_SETTING['IS_DEBUG']) 
